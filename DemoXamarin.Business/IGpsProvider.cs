54b70910985887fa09e30aef3662c7866c882438
Joffrey Kern <joffrey.kern@live.fr>
2016-01-24 18:20:34 +0100

    refactor: Add gps provider interface

---
+++ DemoXamarin.Business/IGpsProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoXamarin.Business
+{
+    public interface IGpsProvider
+    {
+        bool GeolocationAvailable { get; }
+        GeoCoordinate GetCurrentPosition { get; }
+        Task RefreshPositionAsync();
+    }
+
+    public class GeoCoordinate
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
